164ee0704a165fc680c489449840b71b4f8e3b33
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-12-20 01:19:26 +0530

    App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Project setup for dev-portfolio website</p>
-      </header>
+    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+      <Card />
+      <About />
+      <Skills />
+      <Footer />
     </div>
   );
 }
