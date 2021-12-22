eed929526b84ac5d7ead9dd0db11927458d35316
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-12-22 13:03:02 +0530

    imported contact me and integrated AOS library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import About from "./components/About";
 import Card from "./components/Card";
+import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      once: true,
+    });
+  });
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <Card />
-      <About />
+      <div data-aos="fade-down" data-aos-duration="1000">
+        <Card />
+      </div>
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400">
+        <About />
+      </div>
       <Skills />
+      <ContactMe />
       <Footer />
     </div>
   );
